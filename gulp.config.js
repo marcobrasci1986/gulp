2cbdfce3d336fbc3aa57651b091728f95a7d4b4d
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-02-24 09:33:24 +0100

    adding automated bower

--- gulp.config.js
+++ gulp.config.js
@@ -1,5 +1,7 @@
 module.exports = function () {
     var client = './src/client/';
+    var clientApp = client + 'app/';
+
     var config = {
         temp: './.tmp/',
 
@@ -12,8 +14,34 @@ module.exports = function () {
             './src/**/*.js',
             './*.js'
         ],
+        client: client,
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js',
+        ],
 
-        less: client + 'styles/styles.less'
+        less: client + 'styles/styles.less',
+
+        /**
+         * Bower and npm locations
+         */
+        bower : {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        }
+    };
+
+    config.getWireDepDefaultOptions = function () {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+
+        return options;
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var del = require('del');
-var config = require('./gulp.config')();
+var config = require('./gulp.config')(); // () means execute immediately
 
 var $ = require('gulp-load-plugins')({lazy: true});
 
@@ -16,32 +16,52 @@ gulp.task('vet', function () {
         .pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('styles',['clean-styles'], function () {
-    log('Compiling less --> CSS');
-
-    return gulps
-        .src(config.less)
-        .pipe($.less())
-        .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
-        .pipe(gulp.dest(config.temp));
-});
-
 gulp.task('clean-styles', function (done) {
     var files = config.temp + '**/*.css';
     clean(files, done);
 
 });
 
+gulp.task('styles',['clean-styles'],  function () {
+    log('Compiling less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest(config.temp));
+});
+
+
 gulp.task('less-watcher', function () {
     gulp.watch([config.less], ['styles']);
 });
+
+/**
+ * 1. get index.html file
+ * 2. Find bower files in right order
+ * 3. Find and inject custom js (first module files, then normal js, exclude spec files)
+ */
+gulp.task('wiredep', function () {
+    var options = config.getWireDepDefaultOptions();
+    var wiredep = require('wiredep').stream;
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js)))
+        .pipe(gulp.dest(config.client));
+});
+
 
 
 
 ////////////////////////
 function clean(path, done) {
     log('Cleaning: ' + $.util.colors.blue(path));
-    del(path, done);
+    del(path);
+    done();
 }
 function log(msg) {
     $.util.log($.util.colors.blue(msg));
